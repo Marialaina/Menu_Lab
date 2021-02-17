0179e03b098701a673c5316c485a57152562e93e
Marialaina <pmarialaina@gmail.com>
2021-02-17 11:34:55 -0700

    Add files via upload
    Added sub menu

--- jQuer/script.js
+++ jQuer/script.js
@@ -17,12 +17,14 @@ let menuLinks = [
 ];
 
 menuLinks.forEach(link => {
-    const aTags = document.createElement("a");
-    aTags.href = link.href;
-    const text = document.createTextNode(link.text);
-    aTags.append(text);
-    console.log(aTags)
-    topMenuEl.append(aTags)
+    //Creating an a element for the html
+    const aEl = document.createElement("a");
+    //adding the link tag from the object
+    aEl.setAttribute('href', link.href);
+    //Setting the a tag to have the value of text in the object
+    aEl.textContent = link.text;
+    //appending my a tag.
+    topMenuEl.append(aEl);
     
   
 })
